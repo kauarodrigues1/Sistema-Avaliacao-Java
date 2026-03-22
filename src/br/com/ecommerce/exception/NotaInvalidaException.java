9831c122bca49543600177e670f975605baef4f4
Felipe <felpsmarceli@gmail.com>
2026-03-22 18:23:02 -0300

    Exceptions added

---
+++ src/br/com/ecommerce/exception/NotaInvalidaException.java
@@ -0,0 +1,8 @@
+package br.com.ecommerce.exception;
+
+public class NotaInvalidaException extends Exception {
+    public NotaInvalidaException(String mensagem){
+        super(mensagem);
+    }
+
+}
