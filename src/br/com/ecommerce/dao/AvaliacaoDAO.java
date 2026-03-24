c53dee4f491ca9858f2890ad88d601cda5854ae9
Matheus Plati <rm559813@fiap.com.br>
2026-03-23 21:49:10 -0300

    Implement AvaliacaoDAO with salvar method

--- src/br/com/ecommerce/dao/AvaliacaoDAO.java
+++ src/br/com/ecommerce/dao/AvaliacaoDAO.java
@@ -1,1 +1,29 @@
+package br.com.ecommerce.dao;
 
+import br.com.ecommerce.model.Avaliacao;
+import br.com.ecommerce.exception.NotaInvalidaException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AvaliacaoDAO {
+
+    public void salvar(Avaliacao avaliacao) throws NotaInvalidaException {
+        try {
+            Connection conn = ConnectionFactory.getConnection();
+
+            String sql = "INSERT INTO avaliacao (id_cliente, nota_loja, nota_produto) VALUES (?, ?, ?)";
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, avaliacao.getIdCliente());
+            stmt.setInt(2, avaliacao.getNotaLoja());
+            stmt.setInt(3, avaliacao.getNotaProduto());
+
+            stmt.execute();
+
+        } catch (SQLException e) {
+            throw new NotaInvalidaException("Erro ao salvar avaliação no banco.");
+        }
+    }
+}
