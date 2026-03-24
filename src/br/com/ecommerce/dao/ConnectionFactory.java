f90161c15bade04f0569790e4c0913da2b1bd43f
Matheus Plati <rm559813@fiap.com.br>
2026-03-23 21:48:05 -0300

    Implement ConnectionFactory for database connections

--- src/br/com/ecommerce/dao/ConnectionFactory.java
+++ src/br/com/ecommerce/dao/ConnectionFactory.java
@@ -1,1 +1,23 @@
+package br.com.ecommerce.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    private static Connection connection;
+
+    private ConnectionFactory() {}
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null) {
+            connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/ecommerce",
+                "root",
+                "senha"
+            );
+        }
+        return connection;
+    }
+}
